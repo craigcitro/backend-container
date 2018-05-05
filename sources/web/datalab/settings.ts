20b538b1fcaf56c81a6751f8b070da3f9ce0d27a
Craig Citro <craigcitro@google.com>
2018-05-04 23:22:30 -0700

    Drop several more unused config settings.

--- sources/web/datalab/settings.ts
+++ sources/web/datalab/settings.ts
@@ -66,9 +66,6 @@ export function loadAppSettings(): common.AppSettings {
 
     const settings = <common.AppSettings>JSON.parse(fs.readFileSync(settingsPath, 'utf8') || '{}');
     settings.versionId = process.env['DATALAB_VERSION'] || '';
-    if (process.env['DATALAB_CONFIG_URL']) {
-      settings.configUrl = process.env['DATALAB_CONFIG_URL'];
-    }
     if (!fs.existsSync(basePathFile)) {
       _log('Base path setting file not found, falling back to empty path.');
       settings.datalabBasePath = '';
